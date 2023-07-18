3bc24d73009189478d893f1aca589f53c6cb104d
joshbaughmusic <josh.baugh92@gmail.com>
2023-07-18 14:40:29 -0500

    start profile list

--- src/components/profile/ProfilesAllList.js
+++ src/components/profile/ProfilesAllList.js
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react"
 
 export const ProfilesAllList = () => {
     const [allProfiles, setAllProfiles] = useState()
+    const [tags, setTags] = useState()
+    const [subGenres, setSubGenres] = useState()
 
     useEffect(() => {
-        fetch(`http://localhost:8088/profiles`)
+        fetch(`http://localhost:8088/profiles?_expand=user&_expand=primaryGenre&_embed=profileSubGenres&_embed=profileTags`)
         .then(res => res.json())
         .then(data => {
             setAllProfiles(data)
