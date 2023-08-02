42af6db42edebf8a6e89c59fb4c27935fecccd37
joshbaughmusic <josh.baugh92@gmail.com>
2023-08-02 13:40:30 -0500

    stuff

--- src/components/profile/NewPhoto.js
+++ src/components/profile/NewPhoto.js
@@ -19,7 +19,20 @@ export const NewPhoto = ({ closeNewPhoto, myProfileId, setMedia, }) => {
             url: newPhoto.url
         }
 
-        if (newPhoto.url !== "") {
+        function isValidUrl(str) {
+            const pattern = new RegExp(
+              '^([a-zA-Z]+:\\/\\/)?' + // protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+                '(\\#[-a-z\\d_]*)?$', // fragment locator
+              'i'
+            );
+            return pattern.test(str);
+          }
+
+        if (isValidUrl(newPhoto.url)) {
             return fetch(`http://localhost:8088/media`, {
                 method: "POST",
                 headers: {
