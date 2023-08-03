4c94fdac43c4ce210b8ed8e584e6e5417b7f6031
joshbaughmusic <josh.baugh92@gmail.com>
2023-08-03 18:44:27 -0500

    some fixed

--- src/components/posts/PostProfile.js
+++ src/components/posts/PostProfile.js
@@ -344,7 +344,7 @@ export const PostProfile = ({ userName, userId, postId, userPicture, postBody, p
 
 
                                             {
-                                                commentsWithUsers.map(comment => {
+                                                commentsWithUsers.map((comment, index) => {
                                                     if (parseInt(comment.commentObj.postId) === parseInt(postId)) {
                                                         return <Comment
                                                             fullCommentObj={comment}
@@ -355,7 +355,7 @@ export const PostProfile = ({ userName, userId, postId, userPicture, postBody, p
                                                             commentName={comment.userObj.name}
                                                             commentPicture={comment.userObj.profiles[0].picture}
                                                             commentProfileId={comment.userObj.profiles[0].id}
-                                                            key={`commentCard--${comment.commentObj.Id}`}
+                                                            key={`comment--${index}`}
                                                             commentKey={`commentCard--${comment.commentObj.Id}`}
                                                             getAllComments={getAllComments}
                                                         />
@@ -486,7 +486,7 @@ export const PostProfile = ({ userName, userId, postId, userPicture, postBody, p
 
 
                                             {
-                                                commentsWithUsers.map(comment => {
+                                                commentsWithUsers.map((comment, index) => {
                                                     if (parseInt(comment.commentObj.postId) === parseInt(postId)) {
                                                         return <Comment
                                                             fullCommentObj={comment}
@@ -498,6 +498,7 @@ export const PostProfile = ({ userName, userId, postId, userPicture, postBody, p
                                                             commentPicture={comment.userObj.profiles[0].picture}
                                                             commentProfileId={comment.userObj.profiles[0].id}
                                                             key={`commentCard--${comment.commentObj.Id}`}
+                                                            key={`comment--${index}`}
                                                             commentKey={`commentCard--${comment.commentObj.Id}`}
                                                             getAllComments={getAllComments}
                                                         />
